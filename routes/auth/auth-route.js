8937610407fdddffd90974fbced95bc78ab25a40
SANTOSH146 <santoshk6r@gmail.com>
2025-09-03 12:35:29 +0530

    recheck

--- routes/auth/auth-route.js
+++ routes/auth/auth-route.js
@@ -19,5 +19,15 @@ const loginLimiter = rateLimit({
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.get("/check-auth", authMiddleware, (req, res) => {
+  const user = req.user;
+  res.status(200).json({
+    success: true,
+    message: "Authenticated user!",
+    user,
+  });
+});
+
 
 module.exports = router;
