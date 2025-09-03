fa82897b654ba69b796a2722cd8bb83e1abf4514
SANTOSH146 <santoshk6r@gmail.com>
2025-09-03 12:17:22 +0530

    developed middleware functionality

--- controllers/auth/auth-cotroller.js
+++ controllers/auth/auth-cotroller.js
@@ -83,6 +83,33 @@ const loginUser = async (req, res) => {
 };
 //logout
 
+const logoutUser = (req, res) => {
+  res.clearCookie("token").json({
+    success: true,
+    message: "Logged out Successfully",
+  });
+};
+
 //auth middleware
 
-module.exports = { registerUser,loginUser };
+const authMiddleware = async (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token)
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized User!",
+    });
+
+  try {
+    const decoded = jwt.verify(token, "CLIENT_SECRET_KEY");
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Unauthorized User!",
+    });
+  }
+};
+
+module.exports = { registerUser, loginUser, logoutUser, authMiddleware };
