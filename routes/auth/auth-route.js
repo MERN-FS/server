bdd62becc9605b5a0f67b5e65474b1770c1a94fa
SANTOSH146 <santoshk6r@gmail.com>
2025-09-03 13:03:11 +0530

    error fixes

--- routes/auth/auth-route.js
+++ routes/auth/auth-route.js
@@ -1,21 +1,12 @@
 const express = require("express");
-const rateLimit = require("express-rate-limit");
 const {
   registerUser,
   loginUser,
   logoutUser,
   authMiddleware,
-} = require("../../controllers/auth/auth-cotroller");
-const router = express.Router();
+} = require("../../controllers/auth/auth-controller");
 
-// Limit repeated login attempts to 5 per 15min window per IP
-const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5,
-  message: "Too many login attempts from this IP, please try again after 15 minutes.",
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
+const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
@@ -28,6 +19,5 @@ router.get("/check-auth", authMiddleware, (req, res) => {
     user,
   });
 });
-
 
 module.exports = router;
